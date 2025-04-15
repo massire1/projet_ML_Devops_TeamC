6a394584d2e09511184f6d852f2100baa53412c4
massire1 <diamasta@gmail.com>
2025-04-15 21:55:03 +0000

    presentation massire

---
+++ tache1.py
@@ -0,0 +1,8 @@
+#Script python qui affiche ma présentation
+print(" - * - "*10)
+print("\t Ma Présentation ")
+print(" - * - "*10)
+print("Je m'appelle Massiré DIA")
+print("Je suis étudiant au DIT Master 1 IA JAN 25")
+print("Je suis amateur de foot, et de basketball")
+print(" - - "*10)
