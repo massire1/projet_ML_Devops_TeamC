8c714eedb929bbab1624a0d5b8573cdb6d828753
Adams <adamasarr7837@gmail.com>
2025-04-16 01:34:07 +0000

    je viens de finir ma tache

---
+++ tache-adama.py
@@ -0,0 +1,8 @@
+#Script python qui affiche ma présentation
+print(" - * - "*10)
+print("\t Ma Présentation ")
+print(" - * - "*10)
+print("Je m'appelle Adana sarr")
+print("Je suis étudiant au DIT Master 1 IA JAN 25")
+print("Je suis amateur de lamb mbapatte serere, et de basketball")
+print(" - - "*10)
