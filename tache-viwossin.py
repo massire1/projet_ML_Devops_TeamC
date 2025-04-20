f21822f11fe20978fc9b67b6240ffde46ade4df5
vdegboe <dgildas2@yahoo.fr>
2025-04-20 21:21:08 +0100

    Actualisation du fichier

---
+++ tache-viwossin.py
@@ -0,0 +1,8 @@
+#Script python qui affiche ma présentation
+print(" - * - "*10)
+print("\t Ma Présentation ")
+print(" - * - "*10)
+print("Je m'appelle Viwossin DEGBOE")
+print("Je suis étudiant au DIT Master 1 IA JAN 25")
+print("J'aime l'informatique, la musique et les voyages")
+print(" - - "*10)
